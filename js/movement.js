d4f2690dcbe5bbbd424f7a5ae52044c876162779
SamTime101 <116491957+samTime101@users.noreply.github.com>
2023-08-07 18:44:29 +0545

    bug fixed

--- js/movement.js
+++ js/movement.js
@@ -11,22 +11,26 @@ let mariowy = document.querySelector('#marioy')
 
 function animate_mario() {
     
-    requestAnimationFrame(animate_mario)
     c.clearRect(0, 0, canvas.width, canvas.height)
 
     c.fillRect(mario.x, mario.y, mario.w, mario.h)
 
     mario.x += mario.speedX
-    if (mario.y + mario.h  <= canvas.height) {
-        mario.y += mario.speedY
-        mario.speedY += gravity
-    } else {
-        mario.speedY = 0
-        mario.y = canvas.height - mario.h
-    }
-    //ADDED FOR DEBUGGING DONT REMOVE
-    marioswy.textContent = `X position:  ${mario.x}`
+if (mario.y + mario.h <= canvas.height) {
     mariowy.textContent = `Y position:  ${mario.y}`
+    mario.y += mario.speedY
+    mario.speedY += gravity
+}
+  
+else {
+    mario.canJump = true 
+    mario.onGround = true 
+    mario.y = canvas.height - mario.h
+    mario.speedY = 0;
+    marioswy.textContent = `X position:  ${mario.x}`
+}
+    //ADDED FOR DEBUGGING DONT REMOVE
+    requestAnimationFrame(animate_mario)
 }
 
 animate_mario()
